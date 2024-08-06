4f63ddf24d8dfd9e342c61c70666bfd850c6a5f2
AistleitnerFabian <fabian.aistleitner@gmail.com>
2024-08-06 21:45:20 +0200

    add remove item from array in props option

--- components/prop-options/prop-options.vue
+++ components/prop-options/prop-options.vue
@@ -2,16 +2,18 @@
   <div class="h-full">
     <scroll-area class="h-full">
       <div class="p-3">
-        <p class="sticky text-sm mb-2">Prop Options</p>
+        <p class="sticky text-sm">Prop Options</p>
+        <separator class="my-2"/>
         <div v-if="selectedProp?.key">
-
           <!-- Object -->
           <div v-if="selectedProp.subSchema.type == 'object'">
             <div v-for="(value, key) in selectedProp.subSchema.properties" :key="key">
               <div v-if="value.type == 'object'">
-                <Button class="w-full" variant="outline" v-on:click="selectProp(`${selectedProp!.key}.${key}`, value)">{{ `Open child "${key}"` }}</Button>
+                <Button class="w-full" variant="outline" v-on:click="selectProp(`${selectedProp!.key}.${key}`, value)">
+                  {{ `Open child "${key}"` }}
+                </Button>
               </div>
-              <div v-else>
+              <div v-else class="my-2">
                 <Label>{{ `${selectedProp!.key}.${key}` }}</Label>
                 <Input placeholder="null" :default-value="getNestedValue(`${selectedProp!.key}.${key}`)"
                        v-on:update:modelValue="(val: string) => updatePropValue(`${selectedProp!.key}.${key}`, val)"/>
@@ -21,15 +23,22 @@
 
           <!-- Array -->
           <div v-else-if="selectedProp.subSchema.type == 'array'">
-            <div v-for="(value, i) in getNestedValue(selectedProp.key, [])" :key="selectedProp!.key">
+            <div v-for="(value, i) in getNestedValue(selectedProp.key, [])" :key="selectedProp!.key" class="my-2">
               <Label>{{ `${selectedProp?.key}[${i}]` }}</Label>
-              <Input placeholder="null" :default-value="value"
-                     v-on:update:modelValue="(val: string) => updatePropValue(`${selectedProp?.key}`, updateArray(getNestedValue(selectedProp!.key), i, val))"/>
+              <div class="relative gap-0" :key="value">
+                <Input placeholder="null" class="pr-20" :default-value="value"
+                       v-on:update:modelValue="(val: string) => updatePropValue(`${selectedProp?.key}`, updateArray(getNestedValue(selectedProp!.key), i, val))"/>
+                <Button class="absolute top-0 right-0 rounded-l-none" variant="outline"
+                        v-on:click="updatePropValue(`${selectedProp.key}`,
+                    removeIndex((getNestedValue(`${selectedProp.key}`, []) as any[]), i))">
+                  {{ `X` }}
+                </Button>
+              </div>
             </div>
             <Button class="w-full" variant="outline"
                     v-on:click="updatePropValue(`${selectedProp.key}`,
                     (getNestedValue(`${selectedProp.key}`, []) as any[]).concat([null]))">
-              {{ `Add Item` }}
+              {{ `Add List Item` }}
             </Button>
 
           </div>
@@ -49,6 +58,7 @@
 <script setup lang="ts">
 
 import {Button} from "~/components/ui/button";
+import {Separator} from "~/components/ui/separator";
 
 const {props, selectedProp, updatePropValue, selectProp} = usePropOptions()
 
@@ -63,6 +73,11 @@ const getNestedValue = (key: string | number, defaultValue?: any) => {
 
 const updateArray = (arr: any[], i: number, val: any) => {
   arr[i] = val
+  return arr
+}
+
+const removeIndex = (arr: any[], i: number) => {
+  arr.splice(i, 1)
   return arr
 }
 </script>
--- components/props-tree/props-tree.vue
+++ components/props-tree/props-tree.vue
@@ -2,6 +2,7 @@
   <div class="h-full">
     <scroll-area class="h-full p-3">
       <p class="sticky text-sm">Document Props</p>
+      <separator class="my-2"/>
       <props-tree-entry :schema="schema.properties" :indent-level="0" root-key-path=""/>
     </scroll-area>
   </div>
@@ -9,6 +10,7 @@
 
 <script setup lang="ts">
 import type {Ref} from "vue";
+import {Separator} from "~/components/ui/separator";
 const {fetchSchema} = usePropOptions()
 const schema: Ref<any> = ref(await fetchSchema())
 </script>
